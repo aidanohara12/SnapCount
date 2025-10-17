4a73623a94e6ae4bc5c3014df46db82930da55cd
Aidan O'Hara <114598541+aidanohara12@users.noreply.github.com>
2025-10-17 11:45:42 -0400

    Got Rid of Alerts

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -3,13 +3,13 @@ import './Header.css'
 import { useEffect, useState } from 'react'
 
 function Header() {
-    const [showUpdate, setShowUpdate] = useState(false)
-    setTimeout(function () {
-          if (!showUpdate) {
-            setShowUpdate(true)
-            alert("There is a brand new version of SnapCount! Go play the game 'Where'd He Play'!");
-        }
-    }, 3000);
+    // const [showUpdate, setShowUpdate] = useState(false)
+    // setTimeout(function () {
+    //       if (!showUpdate) {
+    //         setShowUpdate(true)
+    //         alert("There is a brand new version of SnapCount! Go play the game 'Where'd He Play'!");
+    //     }
+    // }, 3000);
 
     return (
         <div className='header-div'>
